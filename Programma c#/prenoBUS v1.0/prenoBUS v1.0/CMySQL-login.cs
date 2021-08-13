28308147b72d55e61b6170965d76a921fb87a50e
Luca Rossini <luca066ros@gmail.com>
2021-08-13 10:55:22 +0200

    Creazione File e aggiunta login c#

---
+++ Programma c#/prenoBUS v1.0/prenoBUS v1.0/CMySQL-login.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace prenoBUS_v1._0
+{
+    class CMySQL_login
+    {
+        static MySqlConnection conn;
+        string connString;
+        public CMySQL_login()
+        {
+            conn = null;
+            connString = "";
+        }
+        public bool connettiDatabase()
+        {
+            connString = "server=remotemysql.com;user=UZ4rJQ4qBZ;password=xzZFVqol3S;database=UZ4rJQ4qBZ;charset=utf8";
+            //try
+            //{
+                conn = new MySqlConnection(connString);
+                //conn.ConnectionString = connString;
+                conn.Open();
+                return true;
+            /*}
+            catch(Exception)
+            {
+                return false;
+            }    */
+        }
+        public bool disconnettiDatabase()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+                conn = null;
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool controllaCredenziali(string username, string password)
+        {
+            string query = $"SELECT * FROM login WHERE username='{username}' AND password='{password}';";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if(reader.Read())
+                {
+                    return true;
+                }
+            }
+            catch(Exception)
+            { }
+            return false;
+        }
+    }
+}
---
+++ Programma c#/prenoBUS v1.0/prenoBUS v1.0/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prenoBUS_v1._0
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        CMySQL_login database_login;
+        public MainWindow()
+        {
+            InitializeComponent();
+            database_login = new CMySQL_login();
+            connettiDatabase();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(database_login.connettiDatabase().ToString());
+        }
+
+        private void username_MouseEnter(object sender, MouseEventArgs e)
+        {
+            username.Text = "";
+        }
+
+        private void password_MouseEnter(object sender, MouseEventArgs e)
+        {
+            password.Text = "";
+        }
+
+        private void username_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (username.Text == "")
+                username.Text = "Username";
+        }
+
+        private void password_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (password.Text == "")
+                password.Text = "Password";
+        }
+        private void connettiDatabase()
+        {
+            bool check = database_login.connettiDatabase();
+            while(!check)
+            {
+                MessageBoxResult result = MessageBox.Show("Connessione con database non riuscita, riprova", "Errore connessioone", MessageBoxButton.OKCancel);
+                switch (result)
+                {
+                    case MessageBoxResult.OK:
+                        check = database_login.connettiDatabase();
+                        break;
+                    case MessageBoxResult.Cancel:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            string login = username.Text;
+            string pass = password.Text;
+            bool check = false;
+            check = database_login.controllaCredenziali(login, pass);
+            if (check)
+            {
+                MessageBox.Show("Utente verificato!");
+            }
+            else
+            {
+                MessageBox.Show("Utente non presente, riprova!");
+                username.Text = "Username";
+                password.Text = "Password";
+            }
+                
+
+        }
+    }
+}
